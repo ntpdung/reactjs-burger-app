a0b00e9cb068a5a1e6ace727ae77a32325885505
mrs.nguyen2503@gmail.com <mrs.nguyen2503@gmail.com>
2019-02-10 10:27:51 +0700

    Updates

---
+++ precache-manifest.6e9814a2108f04e0dadb60d16ae79a62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactjs-burger-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9d4aef18ad58b38e4208",
+    "url": "/reactjs-burger-app/static/js/runtime~main.9d4aef18.js"
+  },
+  {
+    "revision": "b484c708addfa423fcac",
+    "url": "/reactjs-burger-app/static/js/main.b484c708.chunk.js"
+  },
+  {
+    "revision": "4fc6e4e55b8f33da84f0",
+    "url": "/reactjs-burger-app/static/js/1.4fc6e4e5.chunk.js"
+  },
+  {
+    "revision": "b484c708addfa423fcac",
+    "url": "/reactjs-burger-app/static/css/main.01bb5715.chunk.css"
+  },
+  {
+    "revision": "ecb85d1a6c5a04913bc425e330d4b094",
+    "url": "/reactjs-burger-app/index.html"
+  }
+];
